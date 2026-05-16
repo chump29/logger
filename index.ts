0f1ca04c038fc585b759edee2302acbce7169928
admin <chris@postfmly.com>
2026-05-16 01:24:43 -0500

    fix: using yoctocolors instead of styleText

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
-import { styleText } from "node:util"
-
 import { format } from "date-and-time"
+import { bgBlue, bgRed, cyan, red, white } from "yoctocolors"
 
 const getTime = (): string => {
-  return (
-    styleText("cyan", " [") + styleText("white", format(new Date(), "MM/DD/YYYY @ HH:mm:ss")) + styleText("cyan", "] ")
-  )
+  return cyan(" [") + white(format(new Date(), "MM/DD/YYYY @ HH:mm:ss")) + cyan("] ")
 }
 
 const error = (...objs: unknown[]): void => {
@@ -13,16 +10,8 @@ const error = (...objs: unknown[]): void => {
     return
   }
 
-  console.error(
-    styleText(
-      [
-        "bgRed",
-        "white"
-      ],
-      " ERROR "
-    ) + getTime()
-  )
-  objs.forEach((obj: unknown): void => console.error(styleText("red", " ⤷"), obj))
+  console.error(bgRed(white(" ERROR ")) + getTime())
+  objs.forEach((obj: unknown): void => console.error(red(" ⤷"), obj))
 }
 
 const info = (...objs: unknown[]): void => {
@@ -30,16 +19,8 @@ const info = (...objs: unknown[]): void => {
     return
   }
 
-  console.info(
-    styleText(
-      [
-        "bgBlue",
-        "white"
-      ],
-      " INFO "
-    ) + getTime()
-  )
-  objs.forEach((obj: unknown): void => console.info(styleText("cyan", " ⤷"), obj))
+  console.info(bgBlue(white(" INFO ")) + getTime())
+  objs.forEach((obj: unknown): void => console.info(cyan(" ⤷"), obj))
 }
 
 export { error, info }
