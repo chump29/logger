3d23a1b1547c1d069ced2847d709fa2417f4a70c
Admin <chris@postfmly.com>
2026-08-18 03:16:19 -0500

    chore: update package docs: clean up script

--- docs/README.sh
+++ docs/README.sh
@@ -8,10 +8,6 @@ _bun=~$_bun
 export _bun
 echo -e " • Bun: $_bun"
 
-_typescript=$(jq -r '.overrides.typescript // "❓"' ../package.json)
-export _typescript
-echo -e " • typescript: $_typescript"
-
 _coverage=-1
 if [ -f "../tests/coverage/lcov.info" ]; then
   _coverage=$(bun run lcov-total ../tests/coverage/lcov.info)
